db43be7a5f8b314670aff1f5a76881db4c96a268
Vincent Nys <vincentnys@gmail.com>
2017-10-03 07:07:07 +0200

    Should take care of undeclared dependencies according to package server.

--- info.rkt
+++ info.rkt
@@ -2,4 +2,10 @@
 (define scribblings '(("scribblings/repeated-application.scrbl")))
 (define collection "repeated-application")
 (define version "0.1")
-(define deps '("sugar"))
+(define deps '("at-exp-lib"
+               "base"
+               "core"
+               "racket-doc"
+               "rackunit-lib"
+               "scribble-lib"
+               "sugar"))
